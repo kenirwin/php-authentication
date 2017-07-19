df4f34179ec0784eea6758986a36c1c0ca8f9a6c
Ken Irwin <kirwin@wittenberg.edu>
2017-07-19 07:47:51 -0700

    replaced semi-colon with commas in Config_Sample.php array

--- classes/Config_Sample.class.php
+++ classes/Config_Sample.class.php
@@ -14,8 +14,8 @@ class Config
         SMTP_HOST = '',
         SMTP_USER = '',
         SMTP_PASS = '',
-        SMTP_PORT = 587; // try 25 (unsecured) if that doesn't work
-        SMTP_SENDER = 'from@example.com';
-        REDIR_PATH = '';
+        SMTP_PORT = 587, // try 25 (unsecured) if that doesn't work
+        SMTP_SENDER = 'from@example.com',
+        REDIR_PATH = ''; //http path for your app, eg. /path/app
 
 }
